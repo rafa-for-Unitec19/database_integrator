81f701088e39aa0899508c5f3f33171da5770e08
Rafael <rafac@unitec.edu>
2020-06-18 16:03:46 -0600

    Funciones para obtener las diferencias con la Base de Datos de Origen

--- src/Integrador/PruebadeConexionPG.java
+++ src/Integrador/PruebadeConexionPG.java
@@ -10,6 +10,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -21,6 +24,9 @@ public class PruebadeConexionPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        String refDate = "2020-06-18 19:28:22.767";
+        
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://database-1.ck0fj32e7uo3.us-east-1.rds.amazonaws.com:5432/HR", "postPower", "waySecure1")) {
  
             System.out.println("Java JDBC PostgreSQL Example");
@@ -32,10 +38,22 @@ public class PruebadeConexionPG {
             Statement statement = connection.createStatement();
             System.out.println("Reading log records...");
             System.out.printf("%-30.30s  %-30.30s%n", "Tabla", "Operacion");
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM audit.bitacora");
-            while (resultSet.next()) {
-                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("tabla"), resultSet.getString("operacion"));
+            String query = "SELECT * FROM audit.bitacora where fecha_hora >= '" + refDate + "'";
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            
+            //Verificador de Sincornizacion
+            if (resultSet.next()) {
+                do {
+                System.out.printf("%-30.30s  %-70.30s%n", resultSet.getString("tabla"), resultSet.getString("operacion"));
+                }while (resultSet.next());
+            }else{
+                System.out.println("Las Tablas estan sincronizadas");
             }
+            
+            
+            
+            
  
         } /*catch (ClassNotFoundException e) {
             System.out.println("PostgreSQL JDBC driver not found.");
@@ -44,6 +62,19 @@ public class PruebadeConexionPG {
             System.out.println("Connection failure.");
             e.printStackTrace();
         }
+        
+        
+//        try {
+//            //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+//            Date parsedDate = new Date();
+//            Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+//            System.out.println(timestamp.toString());
+//        } catch (Exception e) { //this generic but you can control another types of exception
+//            // look the origin of excption 
+//        }
+//        
     }
+    
+    
     
 }
